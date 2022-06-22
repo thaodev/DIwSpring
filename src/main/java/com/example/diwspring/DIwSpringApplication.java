2940e92301fc3dd7510153412c222075506353b1
Thao Nguyen <thaontt1408@gmail.com>
2022-06-21 20:53:36 -0600

    first commit

---
+++ src/main/java/com/example/diwspring/DIwSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.diwspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DIwSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DIwSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/diwspring/DIwSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diwspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DIwSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
