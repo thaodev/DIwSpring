9188ab3987649bfb2ddffaff650038f13f2a8104
Thao Nguyen <thaontt1408@gmail.com>
2022-06-21 22:29:42 -0600

    Add two test files for two created controller

--- src/test/java/com/example/diwspring/controllers/PropertyInjectedControllerTest.java
+++ src/test/java/com/example/diwspring/controllers/PropertyInjectedControllerTest.java
@@ -1,4 +1,23 @@
+package com.example.diwspring.controllers;
+
+import com.example.diwspring.services.BusinessServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PropertyInjectedControllerTest {
-  
+
+    PropertyInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new PropertyInjectedController();
+        controller.businessService = new BusinessServiceImpl();
+    }
+
+    @Test
+    void greeting() {
+        System.out.print(controller.greeting());
+    }
 }
--- src/test/java/com/example/diwspring/controllers/SetterInjectedControllerTest.java
+++ src/test/java/com/example/diwspring/controllers/SetterInjectedControllerTest.java
@@ -1,4 +1,21 @@
+package com.example.diwspring.controllers;
+
+import com.example.diwspring.services.BusinessServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SetterInjectedControllerTest {
-  
+    SetterInjectedController controller;
+    @BeforeEach
+    void setUp() {
+        controller = new SetterInjectedController();
+        controller.setBusinessService(new BusinessServiceImpl());
+    }
+
+    @Test
+    void greeting() {
+        System.out.println(controller.greeting());
+    }
 }
