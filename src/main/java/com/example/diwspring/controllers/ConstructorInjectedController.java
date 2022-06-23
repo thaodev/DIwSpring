f96c07744ddd65a30599cdea91084d1816ab477a
Thao Nguyen <thaontt1408@gmail.com>
2022-06-22 21:53:10 -0600

    Add Contructor InjectedController file

--- src/main/java/com/example/diwspring/controllers/ConstructorInjectedController.java
+++ src/main/java/com/example/diwspring/controllers/ConstructorInjectedController.java
@@ -1,2 +1,18 @@
-package com.example.diwspring.controllers;public class ConstructorInjectedController {
+package com.example.diwspring.controllers;
+
+import com.example.diwspring.services.BusinessService;
+
+public class ConstructorInjectedController {
+
+    private BusinessService businessService;
+
+    public ConstructorInjectedController(BusinessService businessService) {
+        this.businessService = businessService;
+    }
+
+    public String greeting() {
+       return businessService.sayGreeting();
+    }
+
+
 }
